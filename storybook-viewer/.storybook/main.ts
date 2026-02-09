cd99819b3ac49db969434ec7f42f38d5eb6e9093
Albert Boehmler <ajboehmler@gmail.com>
2026-02-09 10:47:00 -0500

    base url for storybook app

--- storybook-viewer/.storybook/main.ts
+++ storybook-viewer/.storybook/main.ts
@@ -21,6 +21,14 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+  viteFinal: async (config) => {
+    // BASE_PATH is set during CI builds for GitHub Pages (e.g., "/trammel/")
+    const basePath = process.env.BASE_PATH;
+    if (basePath) {
+      config.base = `${basePath.replace(/\/$/, '')}/app-storybook/`;
+    }
+    return config;
+  },
 };
 
 export default config;
